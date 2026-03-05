531d2b938396f74e1b77cfd114aef46f8ba9e496
ishanpote <160637054+ishanpote@users.noreply.github.com>
2026-03-05 20:13:07 +0530

    accurate path

--- src/05e_hyperparameter_tuning.py
+++ src/05e_hyperparameter_tuning.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report, accuracy_score
 
 # --- CONFIGURATION ---
-PROCESSED_DATA_PATH = "E:\3rd_year\6SEM\Project\Disaster_NLP_Project\data\processed"
+PROCESSED_DATA_PATH = "E:\\3rd_year\\6SEM\\Project\\Disaster_NLP_Project\\data\\processed"
 MODELS_PATH = "E:\3rd_year\6SEM\Project\Disaster_NLP_Project\models"
 
 # --- 1. LOAD BALANCED DATA ---
