ae0ff4563390b3b1ab21e1d513b320e3c2c4cbdb
ishanpote <ishanvp2005@gmail.com>
2026-08-03 20:02:57 +0530

    cmt rmv

--- src/03_visualizations.py
+++ src/03_visualizations.py
@@ -7,7 +7,6 @@ from collections import Counter
 PROCESSED_DATA_PATH = "E:\\3rd_year\\6SEM\\Project\\Disaster_NLP_Project\\data\\processed"
 VISUALS_PATH = "E:\\3rd_year\\6SEM\\Project\\Disaster_NLP_Project\\visuals"
 
-# Create a folder to save your graphs
 os.makedirs(VISUALS_PATH, exist_ok=True)
 
 # --- 1. LOAD DATA ---
