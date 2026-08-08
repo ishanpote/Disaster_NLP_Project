604a9b96e6965b93e10a3604c5980e7a43846ad0
ishanpote <ishanvp2005@gmail.com>
2026-08-08 22:49:14 +0530

    unnec rmv

--- src/04_advanced_visuals.py
+++ src/04_advanced_visuals.py
@@ -41,7 +41,6 @@ plt.savefig(os.path.join(VISUALS_PATH, "tweet_length_boxplot.png"), dpi=300)
 print("3. Generating Top Bigrams for High Urgency...")
 high_urgency_df = df[df['urgency'] == 'High']['cleaned_text']
 
-# Extract 2-word combinations
 vectorizer = CountVectorizer(ngram_range=(2, 2), stop_words='english', max_features=15)
 bigrams = vectorizer.fit_transform(high_urgency_df)
 bigram_counts = pd.DataFrame(bigrams.sum(axis=0).T, index=vectorizer.get_feature_names_out(), columns=['Freq'])
